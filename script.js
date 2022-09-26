7a6bd450b7bb56e28c104d9bd9f5a9bd97793b5d
gala <andhoumani13003.g@gmail.com>
2022-09-26 18:44:43 +0200

    Suite exo

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+ /* SELECTEURS
 
+ document.querySelector("h4").style.background="red"
+
+const baliseHtml = document.querySelector("h4");
+
+console.log(baliseHtml);
+
+baliseHtml.style.background = "red";
+*/
+
+
+const questionContainer = document.querySelector(".click-event");
+
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn2");
+
+
+
+questionContainer.addEventListener("click", () => {
+	questionContainer.classList.toggle("question-clicked")
+} );
+
+btn1.addEventListener("click",)
 
 
 
