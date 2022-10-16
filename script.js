cf08e5d7bd74c3d9dc2071b966616c9bc1069ce4
gala <andhoumani13003.g@gmail.com>
2022-10-16 21:56:45 +0200

    suite2 cour

--- script.js
+++ script.js
@@ -73,8 +73,23 @@ if(e.key === "j") {
 }
 });
 
+//TODO ---------------------------Scroll Event ------------------------------
+
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  
+  if (window.scrollY > 120) {
+    nav.style.top = 0;
+  } else{
+    nav.style.top = "-50px";
+  }
+});
 
 
+
+//REVIEW ------------------------------- Form Event ------------- 
 
 
 
